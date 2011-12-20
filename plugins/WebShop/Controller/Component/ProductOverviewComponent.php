a69805e2434705814658fe04727a6ab986410225
Maximilian <maximilian.stueber@me.com>
2011-12-20 12:05:57 +0100

    Comments for class ProductOverviewComponent

--- plugins/WebShop/Controller/Component/ProductOverviewComponent.php
+++ plugins/WebShop/Controller/Component/ProductOverviewComponent.php
@@ -1,12 +1,26 @@
 <?php
-
+/**
+ * 
+ * Component for ProductOverview.
+ * 
+ * @author Patrick Zamzow
+ * @version 20.12.2011
+ *
+ */
 class ProductOverviewComponent extends Component {
 	
-	
+	/**
+	 * Method to transfer data from plugin to CMS.
+	 */
 	public function getData($controller, $params)
 	{
+		//LOAD model
 		$controller->loadModel("Products");
+		
+		//GET newest products
 		$products = $controller->Products->find('all', array('limit'=>$params['NumberOfEntries'],'order' => array('created' => 'desc')));
+		
+		//RETURN
 		if ($products != null) {
 			return $products;
 		} else {
@@ -14,6 +28,9 @@ class ProductOverviewComponent extends Component {
 		}
 	}
 	
+	/**
+	 * BeforeFilter.
+	 */
 	public function beforeFilter() {
 		parent::beforeFilter();
 		
